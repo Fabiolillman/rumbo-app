865759560468f8431327e0d04c4e1b9c932e5c54
Fabio Lillman <fabio.lillman@iths.se>
2022-04-06 20:28:07 +0200

    Connected to mongodb

--- server/src/db/model/employee.ts
+++ server/src/db/model/employee.ts
@@ -7,11 +7,11 @@ export interface EmployeeType {
     fullname: string,
 }
 
-const schema = new Schema <EmployeeType>({
+export const schema = new Schema <EmployeeType>({
     email: { type:String, required: true },
     firstname: { type:String, required: true },
     lastname: { type:String, required: true },
-    fullname: { type:String, requred: true }
+    fullname: { type:String, required: true }
 })
 
 const EmployeeModel = model <EmployeeType> ('employee', schema)
